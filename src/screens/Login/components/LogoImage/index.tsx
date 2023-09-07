b7223ea5a22ed203f258d92dde9463357f58a3b0
Henrique Barros <henriquebarrosx@gmail.com>
2023-09-07 01:37:40 -0300

    Corrige exibição da logo na tela de login

--- src/screens/Login/components/LogoImage/index.tsx
+++ src/screens/Login/components/LogoImage/index.tsx
@@ -2,13 +2,13 @@ import React from "react"
 import { View, Image } from "react-native"
 
 import { styles } from "./styles"
-import CompanyLogo from "../../../../../assets/images/logo.png"
+import CompanyLogo from "../../../../../assets/images/small-logo.png"
 
 export function LogoImage() {
   return (
     <View style={styles.container}>
       <Image
-        resizeMode="center"
+        resizeMode="contain"
         source={CompanyLogo}
       />
     </View>
