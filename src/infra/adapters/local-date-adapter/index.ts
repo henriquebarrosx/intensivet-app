25e2570a3c476043efacdd23127b3d8f62929e60
Henrique Barros <henriquebarrosx@gmail.com>
2023-10-01 15:19:34 -0300

    Fix: Corrige timezone do adaptador de datas

--- src/infra/adapters/local-date-adapter/index.ts
+++ src/infra/adapters/local-date-adapter/index.ts
@@ -4,7 +4,8 @@ import { ILocalDate, LocalDateFormatEnum } from "./index.gateway"
 
 export const localDate: ILocalDate = {
     format(date: string | number | Date, format: LocalDateFormatEnum): string {
-        if (this.isValid(date)) return moment(date).format(format)
+        if (this.isValid(date)) return moment(localDate.toISO(date)).format(format)
+        throw new Error("Invalid date format")
     },
 
     isToday(date: string | number | Date): boolean {
@@ -14,9 +15,17 @@ export const localDate: ILocalDate = {
 
     isSameYear(date: string | number | Date): boolean {
         if (this.isValid(date)) return moment(date).isSame(moment(), "year")
+        throw new Error("Invalid date format")
     },
 
     isValid(date: string | number | Date): boolean {
         return Number.isInteger(new Date(date).getTime())
-    }
+    },
+
+    toISO(date): Date {
+        return new Date(
+            moment(new Date(date))
+                .toISOString()
+        )
+    },
 }
