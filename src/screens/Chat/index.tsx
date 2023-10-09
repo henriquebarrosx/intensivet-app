216cdb7f0f21c8f8d4fbfec801eba2819f7a183e
Henrique Barros <henriquebarrosx@gmail.com>
2023-10-09 10:09:38 -0300

    Fix: Corrige comportamento de visibilidade das mensagens ao pre-visualizar imagens/vídeos/arquivos

--- src/screens/Chat/index.tsx
+++ src/screens/Chat/index.tsx
@@ -1,5 +1,4 @@
 import React, { Fragment, useEffect } from "react"
-import { useIsFocused } from "@react-navigation/native"
 
 import Messages from "./Messages"
 import { InputArea } from "./InputArea"
@@ -14,12 +13,15 @@ import { useVetCasesContext } from "../../context/VetCasesContext"
 import { FileAttachmentModalProvider } from "../../context/AttachModal"
 import { useVetCaseMessagesContext } from "../../context/VetCaseMessagesContext"
 
-interface Props {
-    route: { params: { videoUri: string } }
+type Props = {
+    route: {
+        params: {
+            videoUri: string
+        }
+    }
 }
 
 function Chat(props: Props) {
-    const isCurrentScreenFocused = useIsFocused()
     const videoUri = props?.route?.params?.videoUri
 
     const vetCaseContext = useVetCaseContext()
@@ -27,17 +29,15 @@ function Chat(props: Props) {
     const vetCaseMessagesContext = useVetCaseMessagesContext()
 
     useEffect(() => {
-        if (isCurrentScreenFocused) {
-            const INITIAL_PAGE = 1
-            const IS_LOADER_VISIBLE = !videoUri
-            vetCaseMessagesContext.findAll(INITIAL_PAGE, IS_LOADER_VISIBLE)
-        }
+        const INITIAL_PAGE = 1
+        const IS_LOADER_VISIBLE = !videoUri
+        vetCaseMessagesContext.findAll(INITIAL_PAGE, IS_LOADER_VISIBLE)
 
         return () => {
             vetCaseMessagesContext.reset()
             vetCasesContext.readMessages(vetCaseContext.data.id)
         }
-    }, [isCurrentScreenFocused])
+    }, [])
 
     return (
         <Fragment>
